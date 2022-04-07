a158faa75c8c4b0ad8ac9a56bb7b28289f2395e2
rcannood <rcannood@users.noreply.github.com>
2022-04-07 15:07:19 +0000

    deploy: e85aff237d855f3975f1c8cbd08e7b2ab8c03af8

--- src/alignment/cellranger_count/test.py
+++ src/alignment/cellranger_count/test.py
@@ -3,25 +3,25 @@ from os import path
 
 ## VIASH START
 meta = {
-    "functionality_name": "cellranger_mkfastq",
+    "functionality_name": "cellranger_count",
     "resources_dir": "resources_test"
 }
 ## VIASH END
 
 print("> Running command")
-input = meta["resources_dir"] + "/cellranger_tiny_bcl_1.2.0/bcl"
-sample_sheet = meta["resources_dir"] + "/cellranger_tiny_bcl_1.2.0/bcl/sample_sheet.csv"
+input = meta["resources_dir"] + "/pbmc_1k_protein_v3/fastqs/pbmc_1k_protein_v3_gex_fastqs"
+reference = meta["resources_dir"] + "/refdata-gex-GRCh38-2020-A"
 output = "test_output"
 
 cmd_pars = [
     "./" + meta["functionality_name"],
     "--input", input,
-    "--sample_sheet", sample_sheet,
+    "--transcriptome", reference,
     "--output", output,
 ]
 out = subprocess.check_output(cmd_pars).decode("utf-8")
 
 print("> Check if file exists")
-assert path.exists(output + "/H35KCBCXY/test_sample"), "No output was created."
+assert path.exists(output + "/filtered_feature_bc_matrix.h5"), "No output was created."
 
 print("> Completed Successfully!")
--- src/demux/cellranger_mkfastq/script.sh
+++ src/demux/cellranger_mkfastq/script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 ## VIASH START
-par_input=resources_test/cellranger_tiny_bcl_1.2.0/bcl_data
-par_sample_sheet=resources_test/cellranger_tiny_bcl_1.2.0/bcl_data/sample_sheet.csv
+par_input='resources_test/cellranger_tiny_bcl_1.2.0/bcl'
+par_sample_sheet='resources_test/cellranger_tiny_bcl_1.2.0/bcl/sample_sheet.csv'
 par_output=foo
-meta_resources_name=foo
 par_input=`realpath $par_input`
 par_sample_sheet=`realpath $par_sample_sheet`
 par_output=`realpath $par_output`
@@ -27,10 +26,6 @@ else
   input_dir="$par_input"
 fi
 
-# create output dir, if need be
-if [ ! -d "$par_output" ]; then
-  mkdir -p "$par_output"
-fi
 
 # add additional params
 extra_params=( )
@@ -57,10 +52,12 @@ cellranger mkfastq \
   --run "$par_input" \
   "${extra_params[@]}" \
   --disable-ui
-  # --output-dir "$work_dir" \
 cd "$orig_pwd"
 
-ls -l "$work_dir/$id/outs/"
-if [ -d "$work_dir/$id/outs/" ]; then
-  mv "$work_dir/$id/outs/fastq_path" "$par_output/"
+echo "Copying output"
+if [ -d "$work_dir/$id/outs/fastq_path/" ]; then
+  if [ ! -d "$par_output" ]; then
+    mkdir -p "$par_output"
+  fi
+  mv "$work_dir/$id/outs/fastq_path/"* "$par_output"
 fi
